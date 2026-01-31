0447199b6ee9ce49d1a75ff0fde7cb93895f0441
tejasw <stejasw007@gmail.com>
2026-01-31 15:41:49 +0530

    Fix: Added sys.path hack to api/index.py for ModuleNotFoundError

--- api/index.py
+++ api/index.py
@@ -1,4 +1,11 @@
+import sys
+import os
+
+# Add current directory to path so imports work
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 from mangum import Mangum
 from server import app
+
 handler = Mangum(app, lifespan='off')
 
